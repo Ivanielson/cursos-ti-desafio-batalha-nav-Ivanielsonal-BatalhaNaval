ab0dda760bacecf635be22766bec5a9882cd4602
Ivanielson Cabral <icabral@pagbank.com>
2026-03-06 22:53:55 +0000

    code refactoring

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -27,9 +27,6 @@ int main() {
     int shipOne[3] = {3, 3, 3};
     int shipTwo[3] = {3, 3, 3};
 
-    // Calcula o tamanho do tabuleiro: 400 bytes (total) / 40 bytes (uma linha) = 10 linhas
-    int sizeBoard = sizeof(board) / sizeof(board[0]);
-
     printf("TABULIERO BATALHA NAVAL\n");                // Imprime um "Título" para o tabuleiro
     printf("   ");                                      // Imprime 3 espaços vázios para alinhar as letras(cabeçalho) das colunas
 
@@ -40,12 +37,17 @@ int main() {
 
     printf("\n");                                       // Imprime uma nova linha para separar o cabeçalho do tabuleiro
     
+    // Calcula o tamanho do tabuleiro: 400 bytes (total) / 40 bytes (uma linha) = 10 linhas
+    int sizeLineBoard = sizeof(board) / sizeof(board[0]);
+
     // imprime o tabuleiro
-    for (int line = 0; line < sizeBoard; line++) {
+    for (int line = 0; line < sizeLineBoard; line++) {
         printf("%.2i", line + 1);                       // Imprime os números das linhas, formatados para ter 2 dígitos (ex: 01, 02, ..., 10)
         printf(" ");                                    // Imprime um espaço para separar os números das linhas do tabuleiro
 
-        for (int column = 0; column < sizeBoard; column++) {
+        // Calcula o tamanho da linha do tabuleiro: 40 bytes (uma linha) / 4 bytes (um elemento) = 10 colunas
+        int sizeColumnBoard = sizeof(board[line]) / sizeof(board[line][0]);
+        for (int column = 0; column < sizeColumnBoard; column++) {
             if (line == 3 && column <= 2) {             // Imprime um "3" para representar um navio de 3 posições na linha 4 (índice 3) e colunas 1, 2 e 3 (índices 0, 1 e 2)
                 int indexShipOne = 0;
                 printf("%i ", shipOne[indexShipOne]);
